8c6cce5d2f736e78f0970fdca17af26a6e833677
Damenkay <damenkangnaan@yahoo.com>
2022-12-06 18:56:07 +0100

    second commit

--- index.js
+++
@@ -1,7 +0,0 @@
-function validate() {
-    if( document.myForm.fname.value == "" ) {
-        alert( "First Name cannot be empty" );
-        document.myForm.fname.focus() ;
-        return false;
-     }
-}
